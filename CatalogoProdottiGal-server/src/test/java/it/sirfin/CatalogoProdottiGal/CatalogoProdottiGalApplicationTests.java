311ae97c4526fdcb2a0f060e20f1970e95c2150d
ManuelBava <manuelbava1996@gmail.com>
2021-02-15 19:04:48 +0100

    creata struttura server

---
+++ CatalogoProdottiGal-server/src/test/java/it/sirfin/CatalogoProdottiGal/CatalogoProdottiGalApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.CatalogoProdottiGal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoProdottiGalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
